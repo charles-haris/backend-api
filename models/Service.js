c01e66a1c75590e8682de3bad73070b91be55f87
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-02-25 17:19:21 +0000

    Update Service.js

--- models/Service.js
+++ models/Service.js
@@ -7,43 +7,18 @@ const Service = sequelize.define('Service', {
         primaryKey: true,
         autoIncrement: true,
     },
-    auto_url: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    denomination: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    tel: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
+    
     adresse: {
         type: DataTypes.STRING,
         allowNull: true,
     },
-    sociale: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    siret: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
+
     email: {
         type: DataTypes.STRING,
         allowNull: true,
         unique: true
     },
-    logo: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
+   
     created_by: {
         type: DataTypes.INTEGER,
         allowNull: true,
