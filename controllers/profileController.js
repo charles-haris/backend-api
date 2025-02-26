39022907cc52d4731dc7bb7547e4d4b857c0cf87
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-02-26 00:42:40 +0000

    Update profileController.js

---
+++ controllers/profileController.js
@@ -0,0 +1,17 @@
+const Profile = require('../models/Profile');
+
+exports.createRole = async (req, res) => {
+    try {
+       
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.getRoles = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
