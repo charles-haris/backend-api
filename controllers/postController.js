ca7e64d68461388bcb732c069e0c2a67aa15cc87
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-03-01 04:39:31 +0000

    update

---
+++ controllers/postController.js
@@ -0,0 +1,17 @@
+const Post = require('../models/Post');
+
+exports.createPost = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.getPosts = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
